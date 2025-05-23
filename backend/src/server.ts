966561096a48bb266e57e5ead3510c0a6bffefff
shivansh <shivresides@gmail.com>
2025-05-23 22:26:56 +0530

    last commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,25 @@
 import mongoose, { Schema } from "mongoose";
-mongoose.connect("mongodb+srv://shivresides:TjLuNHyDlELlttOM@second-brain.4jq3gmh.mongodb.net/todo-app").then(()=> console.log("connected to db"))
+import { config } from "dotenv";
+
+config();
+
+mongoose.connect(`mongodb+srv://shivresides:${process.env.MONGO_DB_PASS}@second-brain.4jq3gmh.mongodb.net/todo-app`)
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log("DB connection failed:", err));
 
 const userSchema = new Schema({
-    username : String,
-    password : String
-})
-//db pass - TjLuNHyDlELlttOM
+    username: String,
+    password: String
+});
+
 const todoSchema = new Schema({
-    title : String,
+    title: String,
     userId: { type: mongoose.Types.ObjectId, ref: 'UserTable', required: true }
-})
+});
 
-const userModel = mongoose.model("UserTable", userSchema)
-const todoModel = mongoose.model("TodoTable", todoSchema)
+const userModel = mongoose.model("UserTable", userSchema);
+const todoModel = mongoose.model("TodoTable", todoSchema);
 
+const models = { userModel, todoModel };
 
-const models = {userModel, todoModel}
-
-export default models
+export default models;
